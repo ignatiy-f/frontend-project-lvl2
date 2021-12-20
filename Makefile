03a23112ebe12f6dbc50746ea2a96d904d276a0b
Ignatiy Funtikov <ignatiysecond@gmail.com>
2021-12-20 20:53:29 +0300

    step 2

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Makefile
+install:
+	npm ci
+gendiff:
+	node bin/gendiff.js
---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
+
+program.parse();
