fd17ec16886e437a85c6b6c8d07696f96c135738
Ignatiy Funtikov <ignatiysecond@gmail.com>
2022-07-06 19:54:50 +0300

    fixes 2

--- README.md
+++ README.md
@@ -4,18 +4,12 @@
 [![Node CI](https://github.com/ignatiy-f/frontend-project-lvl2/actions/workflows/node.js.yml/badge.svg)](https://github.com/ignatiy-f/frontend-project-lvl2/actions/workflows/node.js.yml)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/7a3fac631a8bb2f0541f/test_coverage)](https://codeclimate.com/github/ignatiy-f/frontend-project-lvl2/test_coverage)
 
-### Difference generator.
+## Difference generator
+
  A "gendiff" is a program that determines the difference between two data structures. This is a popular task for which there are many online services, such as http://www.jsondiff.com/. A similar mechanism is used when outputting tests or when automatically tracking changes in configuration files. Utility features:
  * Support for different input formats: yaml, json
  * Report generation in the form of plain text, stylish and json
 
-This project was built on Hexlet courses for practicing:
-
-* CLI Builder (commander.js)
-* lodash
-* nodejs, FS, path
-* JEST
-* commander
 
 ## Setup
 
--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -1,35 +1,27 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import genDiff from '../src/getdiff.js';
-import { getStylishResult, getPlainResult, getJSformatResult } from '../__fixtures__/result.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const formats = [
+  ['json', 'plain'],
+  ['json', 'stylish'],
+  ['yml', 'stylish'],
+  ['json', 'json'],
+];
+
 const getExpectedPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getExpectedPath(filename), 'utf-8');
 
-test('diffStylishJson', () => {
-  const expectedPath1 = getExpectedPath('file1.json');
-  const expectedPath2 = getExpectedPath('file2.json');
-  expect(genDiff(expectedPath1, expectedPath2)).toEqual(getStylishResult());
-});
-
-test('diffStylishYaml', () => {
-  const expectedPath1 = getExpectedPath('file1.yml');
-  const expectedPath2 = getExpectedPath('file2.yml');
-  expect(genDiff(expectedPath1, expectedPath2)).toEqual(getStylishResult());
-});
-
-test('diffPlainJson', () => {
-  const expectedPath1 = getExpectedPath('file1.json');
-  const expectedPath2 = getExpectedPath('file2.json');
-  expect(genDiff(expectedPath1, expectedPath2, 'plain')).toEqual(getPlainResult());
-});
-
-test('diffFormatJson', () => {
-  const expectedPath1 = getExpectedPath('file1.json');
-  const expectedPath2 = getExpectedPath('file2.json');
-  expect(genDiff(expectedPath1, expectedPath2, 'json')).toEqual(getJSformatResult());
+test.each(formats)('Compare files', (extension, format) => {
+  const expected = readFile(`result-${format}.txt`);
+  const expectedPath1 = getExpectedPath(`file1.${extension}`);
+  const expectedPath2 = getExpectedPath(`file2.${extension}`);
+  const actual = genDiff(expectedPath1, expectedPath2, format);
+  expect(actual).toEqual(expected);
 });
 
 test('toThrow', () => {
